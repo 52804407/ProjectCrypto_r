cd70a799d5df47699735cdf190b680d74cffa273
52804407 <52804407@fsv.cuni.cz>
2024-01-31 12:23:29 +0100

    Added click + portfolios are now functions

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#Main file to run
-#Importing libraries
+# Importing libraries
 import yfinance as yf
 import pandas as pd
 import numpy as np
@@ -9,15 +8,21 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 from dateutil import parser
 import pytz
-import argparse
 import subprocess
-
-# connecting to config.ini
 import configparser
-config = configparser.ConfigParser()
-config.read('config.ini')
-default_config = config['DEFAULT']
+import click
 
+from api_functions import (get_crypto_slugs,
+                           portfolio_manager,
+                           get_crypto_ids,
+                           get_crypto_symbol,
+                           get_market_cap)
+
+                           
+
+from portfolio_functions import (calculate_equal_weights,
+                                 calculate_global_minimum_variance,
+                                 calculate_value_weights)
 
 def get_portfolio_choice():
     print("Choose a portfolio:")
@@ -35,17 +40,56 @@ def get_portfolio_choice():
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-if __name__ == "__main__":
+@click.command()
+@click.argument('currencies', nargs=-1)
+@click.option('--list', 'list_currencies', is_flag=True, help='List all available crypto slugs')
+
+def main(currencies, list_currencies):
+    if list_currencies:
+        # If the user wants to list all crypto slugs, call the get_crypto_slugs function
+        crypto_slugs = get_crypto_slugs()
+        print("Available crypto slugs:")
+        for slug in crypto_slugs:
+            print(slug)
+        return
+
+    # If no input provided, use user input or defaults
+    if not currencies:
+        print("Enter up to 5 crypto slugs (Example: bitcoin ethereum solana) (press Enter to skip and use default portfolio):")
+        user_input = input().strip()
+        # Replace non-alphanumeric characters with space
+        user_input = ''.join(char if char.isalnum() or char.isspace() else ' ' for char in user_input)
+        # Split the input into a list of cryptocurrencies
+        currencies = user_input.split()
+        if not currencies:
+            currencies = ['bitcoin', 'ethereum', 'solana']
+
     portfolio_choice = get_portfolio_choice()
 
     if portfolio_choice == 1:
-        portfolio_name = "value_weighted"
+        portfolio_percentages = calculate_value_weights(*currencies)
     elif portfolio_choice == 2:
-        portfolio_name = "equal_weighted"
+        portfolio_percentages = calculate_equal_weights(*currencies)    
     elif portfolio_choice == 3:
-        portfolio_name = "global_minimum_variance"
+        portfolio_percentages = calculate_global_minimum_variance(*currencies)
+        
 
-    subprocess.run(["python", f"{portfolio_name}.py"])
-
-
+    #Print the resulting portfolio
+    print("\nYour portfolio percentages:")
+    labels = []
+    sizes = []
+    for currency, percentage in portfolio_percentages.items():
+        print(f"{currency}: {percentage:.2f}%")
+        labels.append(currency)
+        sizes.append(percentage)
     
+    #Pie chart of resulting portfolio
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax1.axis('equal')
+
+    plt.title("Value-Weighted Portfolio Distribution")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ portfolio_functions.py
@@ -0,0 +1,65 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+
+from api_functions import (get_crypto_slugs,
+                           portfolio_manager,
+                           get_crypto_ids,
+                           get_crypto_symbol,
+                           get_market_cap)
+
+from slugs_mapping_tool import (crypto_mapping_top50,
+                                convert_to_tickers,
+                                convert_to_names)
+
+# Equal_weighted portfolio
+def calculate_equal_weights(*currencies):
+    percentages = {}
+    equal_weight = 1/len(currencies)
+    for currency in currencies:
+        percentages[currency] = equal_weight*100
+    return percentages
+
+# Value_weighted portfolio (based on live market capitalization)
+def calculate_value_weights(*currencies): 
+    # Ensure there are at most 5 currencies
+    if len(currencies) > 5:
+        raise ValueError("Up to 5 currencies are allowed")
+    # Get market cap for each currency
+    market_caps = {}
+    for currency in currencies:
+        try:
+            crypto_id = get_crypto_ids(slug=currency)
+            market_cap_dict = get_market_cap(slug=currency, id=crypto_id)
+            crypto_symbol = get_crypto_symbol(slug = currency)
+            # Skip if market cap is None
+            if market_cap_dict is not None and crypto_symbol in market_cap_dict:
+                market_caps[currency] = market_cap_dict[crypto_symbol]        
+        except ValueError as e:
+            print(e)
+    # If no valid currencies found, print a message
+    if not market_caps:
+        print("No valid currencies found")
+    else:
+        # Calculate total market cap
+        total_market_cap = sum(market_caps.values())
+        # Calculate and print percentages for each currency
+        percentages = {currency: market_cap / total_market_cap * 100 for currency, market_cap in market_caps.items()}
+        return percentages
+
+
+# Global_minimum_variance portfolio
+#convert user input to ticker symbol
+def calculate_global_minimum_variance(*currencies):
+
+
+    #Call the portfolio_manager function with user-selected cryptocurrencies
+    portfolio_percentages = portfolio_manager(*currencies)
+
+    currencies = convert_to_tickers(currencies, crypto_mapping_top50)
+
+    #Convert tickers back to names
+    percentages = {convert_to_names(crypto_mapping_top50).get(ticker, ticker): percentage for ticker, percentage in portfolio_percentages.items()}
+    return percentages
