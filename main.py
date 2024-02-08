6e88e27bda2306bdadcff7652bc48a3dab48a851
tomassamaj <tomassamaj@seznam.cz>
2024-02-08 21:26:55 +0100

    first version of data saving implementation to reduce runtime (before downloading daily data, the code now checks if the data wasnt already downloaded before) + .gitignore file for the csv for saved data

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import configparser
 import click
 import re
 from datetime import datetime, timedelta
+import os
 
 from api_functions import (get_crypto_slugs,
                            portfolio_manager,
@@ -187,21 +188,35 @@ def main(currencies, list_currencies, start_date):
     plt.show()
 
     def get_daily_returns(portfolio_percentages, start_date):
+        #First we need to check if the csv file already exists and is not empty
+        if os.path.exists("saved_daily_return_data.csv") and os.path.getsize("saved_daily_return_data.csv") > 0:
+            existing_data = pd.read_csv("saved_daily_return_data.csv", index_col=0, parse_dates=True)
+        else:
+            #In case the csv file is empty/doesn't yet exist
+            existing_data = pd.DataFrame()
+
+        #Formate the dates to fit the yfinance functions
         start_date_formatted = get_start_date_from_period(start_date).strftime('%Y-%m-%d')
         end_date = datetime.now().strftime('%Y-%m-%d')
-
         daily_returns_dict = {}
 
         for currency in portfolio_percentages.keys():
-            symbol = get_crypto_symbol(currency) + "-USD"
+            symbol = get_crypto_symbol(currency) + "-USD" #Append "-USD" as yfinance saves data based on tickers
+            #Check if daily returns data for this currency and time period already exists
+            if currency in existing_data.columns and not existing_data[currency].loc[start_date_formatted:end_date].isnull().all():
+                daily_returns_dict[currency] = existing_data[currency].loc[start_date_formatted:end_date]
+                continue
             try:
                 data = yf.download(symbol, start=start_date_formatted, end=end_date)
                 if not data.empty:
                     daily_returns = data['Close'].pct_change()
+                    existing_data[currency] = daily_returns
                     daily_returns_dict[currency] = daily_returns
             except Exception as e:
                 print(f"Failed to download {symbol}: {str(e)}")
 
+        #Save updated data to the saved_daily_return_data.csv file
+        existing_data.to_csv("saved_daily_return_data.csv")
         return daily_returns_dict
 
     def calculate_weighted_cumulative_returns(daily_returns_dict, portfolio_percentages):
@@ -216,7 +231,7 @@ def main(currencies, list_currencies, start_date):
             cumulative_returns = (1 + portfolio_daily_returns).cumprod() * 100
             return cumulative_returns
 
-        return pd.Series()  # If no data are available return an empty series
+        return pd.Series()
 
     daily_returns_dict = get_daily_returns(portfolio_percentages, start_date)
     cumulative_returns = calculate_weighted_cumulative_returns(daily_returns_dict, portfolio_percentages)
